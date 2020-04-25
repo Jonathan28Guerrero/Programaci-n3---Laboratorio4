f934980795c8ca9f59d0e2c5d9717d67739aadbc
Jonathan28Guerrero <u1803757@unimilitar.edu.co>
2020-04-24 23:22:01 -0500

    Base de interfaces y clases

---
+++ Laboratorio 4/src/laboratorio/pkg4/Subject.java
@@ -0,0 +1,27 @@
+package laboratorio.pkg4;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author User
+ */
+public class Subject {
+    ArrayList Students = new ArrayList();
+    ArrayList StudentAverge = new ArrayList();
+    
+    public void EnterStudent(){
+        
+    }
+    public void LoginByFile(){
+        
+    }
+    public void ScoreAverage(){
+        
+    }
+    public void ExportFileScores(){
+        
+    }
+    public void ExportFileAverges(){
+        
+    }
+}
