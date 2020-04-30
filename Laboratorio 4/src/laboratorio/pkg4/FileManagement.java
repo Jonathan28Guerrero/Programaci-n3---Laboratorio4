40a21475014496422cee29bd465228263b20ee0d
Jonathan28Guerrero <u1803757@unimilitar.edu.co>
2020-04-29 22:53:06 -0500

    Avance FileManagement.java y SubjectParameters (Importar archivo y clasificarlo)

---
+++ Laboratorio 4/src/laboratorio/pkg4/FileManagement.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio.pkg4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class FileManagement {
+    public ArrayList LoginByFile(File FileStudents) throws FileNotFoundException{
+        ArrayList TableStudents = new ArrayList();
+        try(Scanner ScanFile = new Scanner(FileStudents);){
+            while (ScanFile.hasNextLine()){
+                ArrayList Values = SplitColumns(ScanFile.nextLine());
+                TableStudents.add(Values);
+            }
+        }
+        return TableStudents;
+    }
+    public ArrayList SplitColumns(String Row){
+        ArrayList Columns = new ArrayList();
+        try(Scanner RowScanner = new Scanner(Row);){
+            RowScanner.useDelimiter(";");
+                        while(RowScanner.hasNext()) {
+                Columns.add(RowScanner.next());
+            }
+        }
+        return Columns;
+    }
+    public void ExportFileScores(){
+        
+    }
+    public void ExportFileAverges(){
+        
+    }
+}
